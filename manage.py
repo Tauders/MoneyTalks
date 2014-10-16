cca0532d9d5cfd7d5ef1487dbf20318a3e0a8a6a
nocom91 <nastya91_51@mail.ru>
2014-10-16 12:02:49 +0700

    Итак. Скрыть ссылку "Login" для зарегистрированного пользователя пока не удалось. Зато добавила красивую админку ^_^ Если старые логин/пароля админа не будет работать, то вот новые: login: testadmin password: test

--- manage.py
+++ manage.py
@@ -3,7 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE','moneytalks.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moneytalks.settings')
+    os.environ.setdefault('LANG', 'en_US')
 
     from django.core.management import execute_from_command_line
 
