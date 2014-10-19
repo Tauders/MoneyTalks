538112545869acc984aab724655e336d66a28088
bkjbkb <valerim@live.ru>
2014-10-19 17:12:02 +0700

    Добавил CRUD для транзакций

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,11 +1,27 @@
 from django.conf.urls import patterns, url
 
-from transactions.views import TransactionList
+from transactions.views import TransactionListView, TransactionCreateView, TransactionUpdateView, TransactionDeleteView
 
 
 urlpatterns = patterns('',
                        url(
-                           r'^$', TransactionList.as_view(),
+                           regex=r'^$',
+                           view=TransactionListView.as_view(),
                            name='transaction_list'
+                       ),
+                       url(
+                           regex=r'^create/$',
+                           view=TransactionCreateView.as_view(),
+                           name='transaction_create'
+                       ),
+                       url(
+                           regex=r'^delete/(?P<pk>\d+)/$',
+                           view=TransactionDeleteView.as_view(),
+                           name='transaction_delete'
+                       ),
+                       url(
+                           regex=r'^update/(?P<pk>\d+)/$',
+                           view=TransactionUpdateView.as_view(),
+                           name='transaction_update'
                        ),
 )
--- transactions/views.py
+++ transactions/views.py
@@ -1,7 +1,41 @@
-from django.views.generic import ListView
+from braces.views import LoginRequiredMixin
+from django.core.urlresolvers import reverse
+from django.views.generic import DeleteView, ListView, CreateView, UpdateView
+from django.views.generic.edit import ModelFormMixin
+from django.core.exceptions import ValidationError
 
+from transactions.forms import TransactionForm
 from transactions.models import Transaction
 
 
-class TransactionList(ListView):
+class TransactionMixin(LoginRequiredMixin):
     model = Transaction
+    form_class = TransactionForm
+
+    def get_success_url(self):
+        return reverse('transaction_list')
+
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return super(ModelFormMixin, self).form_valid(form)
+
+
+class TransactionListView(TransactionMixin, ListView):
+    pass
+
+
+class TransactionCreateView(TransactionMixin, CreateView):
+    pass
+
+
+class TransactionDeleteView(TransactionMixin, DeleteView):
+    pass
+
+
+class TransactionUpdateView(TransactionMixin, UpdateView):
+    pass
