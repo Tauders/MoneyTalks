035e35bf1cfa1377452297922cc2f67740a07285
nekrofak <Tervin@scalpnet.ru>
2014-10-23 21:27:58 +0700

    Подкрутил криспиформ

--- transactions/forms.py
+++ transactions/forms.py
@@ -1,3 +1,5 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from django.forms import ModelForm
 
 from transactions.models import Transaction
@@ -7,3 +9,14 @@ class TransactionForm(ModelForm):
     class Meta:
         model = Transaction
         exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        super(TransactionForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'transaction-formId'
+        self.helper.form_class = 'form-horizontal'
+        self.helper.form_method = 'post'
+        self.helper.form_action = '.'
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-7'
+        self.helper.add_input(Submit('submit', 'Создать', css_class="col-lg-offset-2"))
