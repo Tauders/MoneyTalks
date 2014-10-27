a3e67cbf876b653d50e301f03a3dee201b0b3797
nocom91 <nastya91_51@mail.ru>
2014-10-27 14:47:05 +0600

    Добавила простой тест для категорий
    Signed-off-by: nocom91 <nastya91_51@mail.ru>

--- categories/tests.py
+++ categories/tests.py
@@ -1,1 +1,18 @@
-# Create your tests here.
+from django.utils.unittest import TestCase
+#from django.test import TestCase
+
+from categories.models import Category
+from django.contrib.auth import get_user_model
+
+class CategoryTest(TestCase):
+
+
+    def test_trycategory(self):
+        cat = Category()
+        cat.parent = None
+        cat.name = 'TestCategory'
+        user = get_user_model().objects.create(
+            username='test',
+        )
+        cat.user = user
+        self.assertEqual(cat.__str__(), 'TestCategory')
