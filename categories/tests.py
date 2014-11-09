18908dab060dec7a897c6102ba2dfdc470596a46
nocom91 <nastya91_51@mail.ru>
2014-11-10 00:24:15 +0600

    Написала тест, но он не запускается.

--- categories/tests.py
+++ categories/tests.py
@@ -1,12 +1,12 @@
-from django.utils.unittest import TestCase
-# from django.test import TestCase
+#from django.utils.unittest import TestCase
+from django.test import TestCase
 
 from categories.models import Category
 from django.contrib.auth import get_user_model
 
 
 class CategoryTest(TestCase):
-    def test_trycategory(self):
+    def test_trycategoryname(self):
         cat = Category()
         cat.parent = None
         cat.name = 'TestCategory'
@@ -15,3 +15,16 @@ class CategoryTest(TestCase):
         )
         cat.user = user
         self.assertEqual(cat.__str__(), 'TestCategory')
+
+    def test_cleancategory(self):
+        user = get_user_model().objects.create(
+            username='test',
+        )
+        cat1 = Category()
+        cat1.name='test'
+        cat1.user = user
+        cat2 = Category()
+        cat2.name='test'
+        cat2.user = user
+        self.assertEqual(Category.clean(cat2), 'Name must be unique')
+
