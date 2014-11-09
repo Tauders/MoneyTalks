148545ee6dc2bdbea4ec6e0b717c59f6dbcf0e32
Tauders <tauders@gmail.com>
2014-11-09 21:54:07 +0600

    Написал 2 юнит теста для place.

--- places/tests.py
+++ places/tests.py
@@ -1,1 +1,20 @@
 # Create your tests here.
+from django.db import IntegrityError
+from django.test import TestCase
+
+from places.models import Place
+
+
+class PlaceTest(TestCase):
+    def test_null_place(self):
+        with self.assertRaises(IntegrityError):
+            p = Place()
+            p.save()
+
+    def test_place_without_user(self):
+        with self.assertRaises(IntegrityError):
+            p = Place.objects.create(
+                name='test',
+            )
+            p.save()
+
