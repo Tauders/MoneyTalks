e0d1bed7e6bf97100cf9fd2641a2a346eabe7344
nocom91 <nastya91_51@mail.ru>
2014-12-04 09:56:39 +0600

    Изменила внешний вид формы добавления категории. Добавила crispy.

--- categories/forms.py
+++ categories/forms.py
@@ -1,3 +1,5 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from django import forms
 from django.forms import ModelForm
 
@@ -14,3 +16,13 @@ class CategoryForm(ModelForm):
         super().__init__(*args, **kwargs)
         self.instance.user = user
         self.fields['parent'] = forms.ModelChoiceField(queryset=Category.objects.filter(user=user), required=False)
+        self.helper = FormHelper()
+        self.helper.form_id = 'place-formId'
+        self.helper.form_class = 'form-horizontal'
+        self.helper.form_method = 'post'
+        self.helper.form_action = '.'
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-7'
+        self.instance.user = user
+        self.helper.add_input(Submit('submit', ('Создать'), css_class="register-submit"))
+
